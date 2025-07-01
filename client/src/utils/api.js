2d8478dd6e952668aae5d8a96cbfd7e60bd984d1
faizrahmang <faizrahmang5@gmail.com>
2025-07-01 10:16:09 +0530

    changed API url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://devdoc-rilu.onrender.com/api',
   timeout: 10000,
   // Do not set Content-Type here; set it per request
 })
