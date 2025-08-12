1d7dc1aac0ec2204115b0b6f5531fd6cedae781d
Durga4444 <durgabhuvaneshwari4444@gmail.com>
2025-08-12 06:51:37 +0530

    frontend

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(limiter);
 app.use(cors({
   //origin: true,
   credentials: true,
-   origin: "http://localhost:5173", // allow your frontend
+   origin: "https://dev-doc-b6kj.vercel.app", // allow your frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
